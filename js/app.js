db39cdc1879fd7687a81be66c3ac91b332b34616
Sofya Nagaslaeva <sofyanagaslaeva@gmail.com>
2017-04-26 23:28:50 -0700

    added controller to change navigation state

--- js/app.js
+++ js/app.js
@@ -3,6 +3,16 @@ angular.module('destination', [])
 
 function locationRouter ($routeProvider) {
   $routeProvider
-    .when('/', {templateUrl: 'partials/destination.html'})
-    .when('/countries', {template: '<h3>Countries</h3>'});
+    .when('/', {templateUrl: 'partials/destination.html',
+      controller: function ($scope) {
+        $scope.setActive('destinations');
+    }})
+    .when('/locations', {template: '<h3>Locations</h3>',
+      controller: function ($scope) {
+        $scope.setActive('location');
+    }})
+    .when('/plan', {template: '<h3>My next trip</h3>',
+      controller: function ($scope) {
+        $scope.setActive('plan');
+    }});
 }
